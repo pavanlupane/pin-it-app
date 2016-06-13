b19780c1d2e6c1863d7d7992dd06650e685c7084
pavanlupane <pavanlupane@gmail.com>
2016-06-13 12:37:00 -0700

    Minor updates

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "200x200>",:small => "150x150>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
